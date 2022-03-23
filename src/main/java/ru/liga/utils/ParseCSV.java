2f396b278c292b3aa1229262080bcd838c3b51d0
ivan-putilin <ivan.putilin95@gmail.com>
2022-03-23 17:11:50 +0300

    Added the ability to parse more files

--- src/main/java/ru/liga/utils/ParseCSV.java
+++ src/main/java/ru/liga/utils/ParseCSV.java
@@ -4,18 +4,15 @@ import ru.liga.model.Currency;
 import ru.liga.model.Rate;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
-import java.util.Locale;
 
 public class ParseCSV {
     public static List<Rate> parse(int lineNumber, List<Rate> data, String fileName) throws IOException {
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(ParseCSV.class.getResourceAsStream(fileName)))) {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(ParseCSV.class.getResourceAsStream(fileName), "windows-1251"))) {
             int count = 0;
             String[] line;
 
@@ -24,13 +21,36 @@ public class ParseCSV {
             }
             while (reader.ready() && count < lineNumber) {
                 line = reader.readLine().split(";");
+
                 Rate rate = new Rate(
-                        LocalDate.parse(line[0], DateTimeUtil.PARSE_FORMATTER),
-                        BigDecimal.valueOf(Double.parseDouble(line[1].replace(',', '.'))),
-                        getCurrency(line[2])
+                        LocalDate.parse(line[1], DateTimeUtil.PARSE_FORMATTER),
+                        new BigDecimal(line[2].replace(',', '.').replace("\"", "")),
+                        getCurrency(line[3])
                 );
                 data.add(rate);
+
                 count++;
+            }
+        }
+        return data;
+    }
+
+    public static List<Rate> parseAll(List<Rate> data, String fileName) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(ParseCSV.class.getResourceAsStream(fileName), "windows-1251"))) {
+            String[] line;
+
+            if (reader.ready()) {
+                reader.readLine();
+            }
+            while (reader.ready()) {
+                line = reader.readLine().split(";");
+
+                Rate rate = new Rate(
+                        LocalDate.parse(line[1], DateTimeUtil.PARSE_FORMATTER),
+                        new BigDecimal(line[2].replace(',', '.').replace("\"", "")),
+                        getCurrency(line[3])
+                );
+                data.add(rate);
             }
         }
         return data;
@@ -47,6 +67,12 @@ public class ParseCSV {
                 break;
             case "Турецкая лира":
                 currency = Currency.TRY;
+                break;
+            case "Армянский драм":
+                currency = Currency.AMD;
+                break;
+            case "Болгарский лев":
+                currency = Currency.BGN;
                 break;
             default:
                 currency = null;
